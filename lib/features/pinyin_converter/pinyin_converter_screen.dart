afd79a68b1db0c7553cda92e5faf9e5ecf9c6340
Michael Chiew <michael@airasia.com>
2024-05-25 15:20:30 +0800

    Add bottom sheet

--- lib/features/pinyin_converter/pinyin_converter_screen.dart
+++ lib/features/pinyin_converter/pinyin_converter_screen.dart
@@ -1,10 +1,12 @@
 import 'package:church_tool/features/pinyin_converter/pinyin_converter.dart';
+import 'package:church_tool/features/pinyin_converter/pinyin_converter_notifier.dart';
 // import 'package:church_tool/features/sample_feature/sample_item_list_view.dart';
 import 'package:church_tool/settings/settings_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 // import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class PinyinConverterScreen extends HookConsumerWidget {
   const PinyinConverterScreen({super.key});
@@ -81,6 +83,67 @@ class PinyinConverterScreen extends HookConsumerWidget {
           ],
         ),
       ),
+      bottomSheet: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                text: 'Made with love by ',
+                style: Theme.of(context).textTheme.bodySmall,
+                children: const <InlineSpan>[
+                  WidgetSpan(
+                    alignment: PlaceholderAlignment.baseline,
+                    baseline: TextBaseline.alphabetic,
+                    child: LinkButton(
+                      urlLabel: 'Michael Chiew',
+                      url: 'https://github.com/michaelchiew08',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class LinkButton extends StatelessWidget {
+  const LinkButton({
+    required this.urlLabel,
+    required this.url,
+    super.key,
+  });
+
+  final String urlLabel;
+  final String url;
+
+  Future<void> _launchUrl(String url) async {
+    final uri = Uri.parse(url);
+
+    await launchUrl(uri);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: TextButton.styleFrom(
+        padding: EdgeInsets.zero,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(0),
+        ),
+        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        visualDensity: VisualDensity.compact,
+        minimumSize: Size.zero,
+        textStyle: Theme.of(context).textTheme.bodySmall,
+      ),
+      onPressed: () {
+        _launchUrl(url);
+      },
+      child: Text(urlLabel),
     );
   }
 }
