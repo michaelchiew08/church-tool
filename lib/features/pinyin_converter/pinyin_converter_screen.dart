4000349186bc1aa8c7d427673ce67c39935dc720
Michael Chiew <michael@airasia.com>
2024-05-27 09:33:34 +0800

    Handled the contextMenuBuilder for web

--- lib/features/pinyin_converter/pinyin_converter_screen.dart
+++ lib/features/pinyin_converter/pinyin_converter_screen.dart
@@ -5,7 +5,6 @@ import 'package:church_tool/settings/settings_controller.dart';
 // import 'package:church_tool/features/sample_feature/sample_item_list_view.dart';
 import 'package:church_tool/settings/settings_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 // import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -26,7 +25,6 @@ class PinyinConverterScreen extends HookConsumerWidget {
     final state = ref.watch(pinyinConverterNotifierProvider);
 
     final newTextFieldArea = useTextEditingController();
-    final selectableTextController = useTextEditingController();
 
     return BaseScaffold(
       settingsController: settingsController,
@@ -57,7 +55,7 @@ class PinyinConverterScreen extends HookConsumerWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Row(
+              Wrap(
                 children: [
                   Text(
                     AppLocalizations.of(context)!.gettingStartedText,
@@ -127,27 +125,15 @@ class PinyinConverterScreen extends HookConsumerWidget {
               ),
               SelectableText(
                 state,
-                onTap: () async {
-                  final selectedText = selectableTextController.selection
-                      .textInside(selectableTextController.text);
-                  const copyAction = PopupMenuItem<String>(
-                    value: 'copy',
-                    child: Row(
-                      children: <Widget>[
-                        Icon(Icons.content_copy),
-                        SizedBox(width: 8),
-                        Text('Copy'),
-                      ],
-                    ),
+                contextMenuBuilder: (context, editableTextState) {
+                  return AdaptiveTextSelectionToolbar.buttonItems(
+                    anchors: editableTextState.contextMenuAnchors,
+                    buttonItems: editableTextState.contextMenuButtonItems
+                      ..removeWhere(
+                        (ContextMenuButtonItem buttonItem) =>
+                            buttonItem.type == ContextMenuButtonType.cut,
+                      ),
                   );
-                  final selectedAction = await showMenu<String>(
-                    context: context,
-                    position: RelativeRect.fill,
-                    items: <PopupMenuEntry<String>>[copyAction],
-                  );
-                  if (selectedAction == 'copy') {
-                    await Clipboard.setData(ClipboardData(text: selectedText));
-                  }
                 },
               ),
             ],
