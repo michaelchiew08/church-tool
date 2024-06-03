8b5aec8e7461a02d3f5570656c0d7fbe4ab3323c
Michael Chiew <michael@airasia.com>
2024-06-03 19:10:33 +0800

    Chinese special characters to be replace with empty string

--- lib/features/pinyin_converter/pinyin_converter_notifier.dart
+++ lib/features/pinyin_converter/pinyin_converter_notifier.dart
@@ -22,8 +22,17 @@ class PinyinConverterNotifier extends Notifier<String> {
     const ls = LineSplitter();
     final linesOfText = ls.convert(text);
     for (final element in linesOfText) {
-      final pinyinState = ref.read(pinyinToneNotifierProvider);
-      if (pinyinState == PinyinFormatType.withToneMark) {
+      final pinyinToneState = ref.read(pinyinToneNotifierProvider);
+
+      if (pinyinToneState == PinyinFormatType.withoutTone) {
+        state = '$state\n${titleCase(
+          PinyinHelper.getPinyin(
+            element,
+          ),
+        )}';
+      }
+
+      if (pinyinToneState == PinyinFormatType.withToneMark) {
         state = '$state\n${titleCase(
           PinyinHelper.getPinyin(
             element,
@@ -32,7 +41,7 @@ class PinyinConverterNotifier extends Notifier<String> {
         )}';
       }
 
-      if (pinyinState == PinyinFormatType.withToneNumber) {
+      if (pinyinToneState == PinyinFormatType.withToneNumber) {
         state = '$state\n${titleCase(
           PinyinHelper.getPinyin(
             element,
@@ -40,15 +49,9 @@ class PinyinConverterNotifier extends Notifier<String> {
           ),
         )}';
       }
-
-      if (pinyinState == PinyinFormatType.withoutTone) {
-        state = '$state\n${titleCase(
-          PinyinHelper.getPinyin(
-            element,
-          ),
-        )}';
-      }
     }
+
+    patchedSpecialCharacters();
   }
 
   void convertTextWithToneMark(String text) {
@@ -109,10 +112,19 @@ class PinyinConverterNotifier extends Notifier<String> {
     return ('\n'.allMatches(str).length + 1).toString();
   }
 
+  void patchedSpecialCharacters() {
+    // 祢 is \u7962 then Ni
+    final regexSpecificCharacter = RegExp(r'\u7962');
+    final regexChineseSpecialCharacter = RegExp(
+      r'[\uff01-\uff0c\uff1a-\uff1f\uff3b-\uff3d\u3001-\u3011]',
+    );
+    state = state.replaceAll(regexChineseSpecialCharacter, '');
+  }
+
   String respondWithSampleText() {
     return '''
-坡合蝴以少男蛋笔上 石呢了课停春谁交目苦棵母力元吃 间羽习 路写升品衣  读兑坡具成活石房杯害科穿对 皮放师吧玩像那具金爪兑或送麻位请故光 共秋九鸟向安的休条苦西笑鼻花戊  \n
-住屋工风躲兄家师习拉南旦世知工爪木完鸡户  你进又片民旦流  是几元 日方自夏  乞亮植到圆友主现民植弓点路什这  王寺口  \n
+坡合蝴以少男蛋笔上，石呢了课停春谁交目苦棵母力元吃 间羽习 路写升品衣。读兑坡具成活石房杯害科穿对 皮放师吧玩像那具金爪兑或送麻位请故光 共秋九鸟向安的休条苦西笑鼻花戊  \n
+住屋工风躲兄家师习拉南旦世知工爪木完鸡户、你进又片民旦流  是几元 日方自夏  乞亮植到圆友主现民植弓点路什这  王寺口  \n
 别寸像日鼻步忍兔根卜话实布京安房笑不只打 古家面课连在那五娘页 急雪牛 奶对言  贝对女进助远羽结立喝抄丢西很左把 ''';
   }
 }
